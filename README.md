a2c7b978abf8cfbf24b762bdcc4d9e6706d77023
echovre <you@example.com>
2021-01-05 20:04:44 -0700

    POST call working

--- README.md
+++ README.md
@@ -1,17 +1,10 @@
 # SpringRestExample
-
-#To Build:
-
+To Build:
 ##git clone <repo>
-
 ##maven clean install
-
 ##mvn -N io.takari:maven:wrapper
 
-#To Run/Test:
-
+To Run/Test:
 ##./mvnw spring-boot:run
-
 ##http://localhost:8080/greeting?name=blah
-
 ##curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"body":"value"}' http://localhost:8080/request
--- src/main/java/com/example/MyApp/HelloWorldController.java
+++ src/main/java/com/example/MyApp/HelloWorldController.java
@@ -2,7 +2,17 @@ package com.example.MyApp;
 
 import java.io.IOException;
 import java.util.UUID;
+
 import javax.ws.rs.core.Response;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,29 +29,41 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RestController
 public class HelloWorldController {
 
-    ObjectMapper mapper = new ObjectMapper();
+	ObjectMapper mapper = new ObjectMapper();
 
 	@PostMapping(path = "/request", consumes = "application/json", produces = "application/json")
 	@ResponseBody
 	public String request(@RequestBody Body payload) throws JsonMappingException, JsonProcessingException {
-	    String uniqueID = UUID.randomUUID().toString();
-	    Request req=new Request(payload.getBody(),uniqueID);
-	    try {
-			makeThirdPartyRequest(req);
-		} catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		String uniqueID = UUID.randomUUID().toString();
+		Request req=new Request(payload.getBody(),uniqueID);
+		boolean success=makeThirdPartyRequest(req);
+		if(success) {
+			return "Initiated request for document:"+payload.getBody();
+		}else {
+			return "FAILED to make request for document:"+payload.getBody();
 		}
-	    return "request initiated for document:"+payload.getBody();
 	}
-	/*
-	 * curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"body":"value"}' http://localhost:8080/request
-request initiated for document:value
-	 */
-	private String makeThirdPartyRequest(Request req) throws IOException, InterruptedException {
-		return "";
-    }
-	
+
+	private boolean makeThirdPartyRequest(Request req){
+		String url="http://example.com/request";
+		try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+			String requestObj=mapper.writeValueAsString(req);
+			HttpPost httpPost = new HttpPost(url);
+			httpPost.setEntity(new StringEntity(requestObj));
+			System.out.println("Executing request " + httpPost.getRequestLine());
+			
+			ResponseHandler<Integer> responseHandler = response -> {
+				return response.getStatusLine().getStatusCode();
+			};
+			int status = httpclient.execute(httpPost, responseHandler);
+			System.out.println("Third party request returned status code:"+status);
+			return status >= 200 && status < 300;
+		} catch (IOException e) {
+			System.out.println("Third party request FAILED!");
+			return false;
+		}
+	}
+
 	@PostMapping("/callback")
 	@ResponseStatus
 	public Response postCallback(@RequestParam(value = "name", defaultValue = "2") String value) {
@@ -53,15 +75,20 @@ request initiated for document:value
 	public Response putCallback(@RequestBody Body callback) {
 		return Response.status(204).build();
 	}
-	
+
 	@GetMapping("/status")
 	public Object doSomething(@RequestParam(value = "id", defaultValue = "") Integer id) {
 		return new Object();
 	}
-	
+
 }
 
 // http://localhost:8080/greeting?name=blah
 // ./mvnw spring-boot:run
 
 //(may have to run mvn -N io.takari:maven:wrapper to get .mvn directory)
+
+/*
+ * curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"body":"value"}' http://localhost:8080/request
+request initiated for document:value
+ */
